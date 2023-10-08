faedc444ede2fb18ceb962f36ae6819b529a93a8
farah alshammari <farah.ntuf@outlook.sa>
2023-10-08 11:54:27 +0300

    first page

--- test8/ContentView.swift
+++ test8/ContentView.swift
@@ -4,20 +4,36 @@
 //
 //  Created by Farah mohammed alshammari on 23/03/1445 AH.
 //
-
 import SwiftUI
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack(alignment:.center) {
+            
+            Image("img")
+            
+                .clipShape(Circle())
+                .overlay{
+                                Circle().stroke(.yellow, lineWidth: 4)
+                    
+                            }
+                       
+            Text(" Farah Alshammari")
+            
+                .font(.title)
+                .foregroundColor(.red)
+                .multilineTextAlignment(.leading)
+            Text("28 years old graphic designer , working in F&B sector ")
+                           .font(.subheadline)
+                           .foregroundStyle(.black)
+            
         }
-        .padding()
+        
     }
 }
+
 
 #Preview {
     ContentView()
