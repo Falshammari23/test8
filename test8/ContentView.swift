9193742fe92572985730e6139611c2a404592766
farah alshammari <farah.ntuf@outlook.sa>
2023-10-08 09:17:11 +0300

    Initial Commit

---
+++ test8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test8
+//
+//  Created by Farah mohammed alshammari on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test8/test8App.swift
@@ -0,0 +1,17 @@
+//
+//  test8App.swift
+//  test8
+//
+//  Created by Farah mohammed alshammari on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct test8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
